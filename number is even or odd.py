c46e496ec7659fafa5bb0c9d95fa50d470cf6ca5
Misba Fathima <134159136+MisbaFathima06@users.noreply.github.com>
2025-08-06 15:35:58 +0000

    Initial commit

---
+++ number is even or odd.py
@@ -0,0 +1,6 @@
+num= int(input("Enter a number:"))
+
+if num%2==0:            #perfectly divisible by 2 and remainder is 0 then
+    print(f"Entered number {num} is an even number")
+else:
+    print(f"Entered number {num} is an odd number")
