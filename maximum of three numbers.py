73c30c7935dda66f6ca93fd8372c54a7aa955217
Misba Fathima <134159136+MisbaFathima06@users.noreply.github.com>
2025-08-06 15:44:11 +0000

    Initial commit

---
+++ maximum of three numbers.py
@@ -0,0 +1,13 @@
+num1= int(input("Enter 1st number:"))
+num2= int(input("Enter 2nd number:"))
+num3= int(input("Enter 3rd number:"))
+
+def largest_num(num1,num2,num3):
+    if num1>=num2 and num1>=num3:
+        print("1st number is largest")
+    elif num2>=num1 and num2>=num3:
+        print("2nd number is largest")
+    else:
+        print("3rd number is largest")
+
+largest_num(num1,num2,num3)
